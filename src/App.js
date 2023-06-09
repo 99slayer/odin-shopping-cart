188748acc13790ae67ccf41dab71f03ff5d2bd74
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-09 16:36:20 -0600

    - Moved the productList state from the ShopPage component to the App component, so the users cart will be the same between page changes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import { NavBar } from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 
-//This is the base page layout**
 const App = () => {
+  const [productList, setProductList] = useState({
+    1: {
+      name: "Toast Master 5000",
+      cost: 125.99,
+      count: 0,
+      image: "toaster-1.jpg",
+    },
+    2: {
+      name: "Toastie Classic",
+      cost: 35.99,
+      count: 0,
+      image: "toaster-2.jpg",
+    },
+    3: {
+      name: "Toastie Classic 2",
+      cost: 40.0,
+      count: 0,
+      image: "toaster-3.jpg",
+    },
+    4: {
+      name: "Toast Box",
+      cost: 50.0,
+      count: 0,
+      image: "toaster-4.jpg",
+    },
+    5: {
+      name: "Toast Box Supreme",
+      cost: 70.0,
+      count: 0,
+      image: "toaster-5.jpg",
+    },
+    6: {
+      name: "Toast Box Lite",
+      cost: 35.99,
+      count: 0,
+      image: "toaster-6.jpg",
+    },
+    7: {
+      name: "Industrial Toaster",
+      cost: 200.0,
+      count: 0,
+      image: "toaster-7.jpg",
+    },
+    8: {
+      name: "The Original",
+      cost: 40.0,
+      count: 0,
+      image: "toaster-8.jpg",
+    },
+    9: {
+      name: "Multi-Toast 5",
+      cost: 85.0,
+      count: 0,
+      image: "toaster-9.jpg",
+    },
+  });
+
   return (
     <>
       <NavBar></NavBar>
-      <Outlet />
+      <Outlet context={[productList, setProductList]} />
     </>
   );
 };
--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 import "../styles/ShopPage.css";
 
 const order = [];
@@ -20,67 +21,7 @@ const changeOrder = (list) => {
 };
 
 export const ShopPage = () => {
-  // Objects are not valid as a React child (found: object with keys {1, 2, 3, 4}). If you meant to render a collection of children, use an array instead. HMMMMMMM
-
-  // May need to move to app component?
-  // Items should remain in your cart even if you leave the shop.
-  // Make cart sticky + only scroll display**
-  const [productList, setProductList] = useState({
-    1: {
-      name: "Toast Master 5000",
-      cost: 125.99,
-      count: 0,
-      image: "toaster-1.jpg",
-    },
-    2: {
-      name: "Toastie Classic",
-      cost: 35.99,
-      count: 0,
-      image: "toaster-2.jpg",
-    },
-    3: {
-      name: "Toastie Classic 2",
-      cost: 40.0,
-      count: 0,
-      image: "toaster-3.jpg",
-    },
-    4: {
-      name: "Toast Box",
-      cost: 50.0,
-      count: 0,
-      image: "toaster-4.jpg",
-    },
-    5: {
-      name: "Toast Box Supreme",
-      cost: 70.0,
-      count: 0,
-      image: "toaster-5.jpg",
-    },
-    6: {
-      name: "Toast Box Lite",
-      cost: 35.99,
-      count: 0,
-      image: "toaster-6.jpg",
-    },
-    7: {
-      name: "Industrial Toaster",
-      cost: 200.0,
-      count: 0,
-      image: "toaster-7.jpg",
-    },
-    8: {
-      name: "The Original",
-      cost: 40.0,
-      count: 0,
-      image: "toaster-8.jpg",
-    },
-    9: {
-      name: "Multi-Toast 5",
-      cost: 85.0,
-      count: 0,
-      image: "toaster-9.jpg",
-    },
-  });
+  const [productList, setProductList] = useOutletContext();
 
   const incrementProduct = (productNum) => {
     if (productList[productNum].count === 999) {
